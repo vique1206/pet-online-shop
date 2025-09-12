7941188de244ad9a383093b0179adb514fd25019
Ya <vladivanov1206@gmail.com>
2025-09-13 02:00:34 +0300

    Добавил визуальную часть, уже добавляет пользователя в БДшку, выгружает товары из БДшки

--- app/main.py
+++ app/main.py
@@ -1,35 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from app.database import Base, engine, SessionLocal
 from app.models import User, Product
 from app.auth import hash_password
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 @app.get("/")
-def root():
-    return {"message": "Добро пожаловать!"}
-
-@app.get("/products")
-def get_products():
+def root(request: Request):
     db = SessionLocal()
     products = db.query(Product).all()
     print(len(products))
     db.close()
-    return [
-        {"id": p.id,
-         "name": p.name,
-         "price": p.price,
-         "count": p.count}
-        for p in products
-    ]
+    return templates.TemplateResponse("products.html", {"request": request,"products": products})
+
+@app.get("/registration")
+def register_page(request: Request):
+    return templates.TemplateResponse("registration.html", {"request": request})
 
 @app.post("/register")
-def register_user(username: str, email: str, password: str):
+def register_user(username: str = Form(...), password: str = Form(...)):
     db = SessionLocal()
     hashed = hash_password(password)
-    user = User(username=username, email=email, password_hash=hashed)
+    user = User(username=username, password_hash=hashed)
     
     db.add(user)
     db.commit()
-    return user
+    return {"message": "ok"}
--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,7 @@
+import bcrypt
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
 from app.database import Base
 
 class User(Base):
@@ -6,11 +9,7 @@ class User(Base):
     
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, index=True, nullable = False)
-    email = Column(String, unique=True, index=True, nullable=False)
     password_hash = Column(String, nullable=False)
     
-    def verify_password(self, password: str) -> bool:
-        return
-        
     def __repr__(self):
-        return f"<User(username={self.username},email={self.email})>"
+        return f"<User(username={self.username},id={self.id})>"
