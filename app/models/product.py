098d2e9d36a17eb9024f0a4e37dc67aeafca2217
Ya <vladivanov1206@gmail.com>
2025-09-12 17:11:09 +0300

    new file:   app/core.py 	new file:   app/database.py 	new file:   app/models/__init__.py 	new file:   app/models/product.py 	new file:   app/models/user.py
     Changes not staged for commit:
    	modified:   .gitignore
    	modified:   app/core.py
    	modified:   app/database.py
    	modified:   app/main.py
    	modified:   app/models/__init__.py
    	modified:   app/models/product.py
    	modified:   app/models/user.py
    	modified:   requirements
    
     Untracked files:
    	app/shop.db

---
+++ app/models/product.py
@@ -0,0 +1,8 @@
+# from sqlalchemy.testing.schema import mapped_column
+#
+# class BookModel(Base)
+#     __tablename__ = "books"
+#
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     title: Mapped[str]
+#     author: Mapper[str]
